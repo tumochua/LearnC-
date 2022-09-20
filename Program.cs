061a3ff6a97bcd9eeffa79b41f43fa1c6265dffd
tumochua <94440913+tumochua@users.noreply.github.com>
2022-09-20 20:44:51 +0700

    init first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+        }
+    }
+}
