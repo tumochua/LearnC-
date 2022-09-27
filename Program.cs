2ccca95a3cc93bedd9554a44e89b31b36c08d151
tumochua <94440913+tumochua@users.noreply.github.com>
2022-09-27 14:59:29 +0700

    tinh dien tich hình tron

--- Program.cs
+++ Program.cs
@@ -10,9 +10,16 @@ namespace LearnC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter username:");
-            string userName = Console.ReadLine();
-            Console.WriteLine("Username is: " + userName);
+            //khai báo bán kính và dien tích hình tròn
+            double S, r;
+            //nhận đầu vào từ người dùng
+            Console.Write("Nhap vao ban kinh r: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            //tính diện tích theo công thức 
+            S = Math.PI * r * r;
+            //hiển thị chu vi vừa được tính ra màn hình
+            Console.Write($"Dien tich hinh tron la: {S}");
+            Console.WriteLine("\n----Chuong trinh nay duoc dang tai Freetuts.net----\n");
         }
     }
 }
