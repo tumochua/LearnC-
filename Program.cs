b994c7dbb747c4b7dd9ccf2e90aa215871841d3c
tumochua <94440913+tumochua@users.noreply.github.com>
2022-10-04 14:52:37 +0700

    check number

--- Program.cs
+++ Program.cs
@@ -10,27 +10,21 @@ namespace LearnC
     {
         static void Main(string[] args)
         {
-            //khai báo bán kính và dien tích hình tròn
-            double number1, number2;
-            double tong;
-            double tich;
-            double hieu;
-            //nhận đầu vào từ người dùng
-            Console.Write("Nhap vao number 1: ");
-            number1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Nhap vao number 2: ");
-            number2 = Convert.ToDouble(Console.ReadLine());
-            //tính diện tích theo công thức 
-            tong = number1 + number2;
-            tich = number1 * number2;
-            hieu = number1 - number2;
-            //hiển thị chu vi vừa được tính ra màn hình
-            Console.Write($"tong: {tong}");
-            Console.Write('\n');
-            Console.Write($"tich: {tich}");
-            Console.Write('\n');
-            Console.Write($"hieu: {hieu}");
-            // Console.WriteLine("\n----Chuong trinh nay duoc dang tai Freetuts.net----\n");
+            double number;
+            // Console.WriteLine in ra màn hình
+            Console.WriteLine("enter to number");
+            // Console.ReadLine() nhập vào từ bàn phím
+            number = Convert.ToDouble(Console.ReadLine());
+            if (number >= 0)
+            {
+                Console.WriteLine("so you can join a group");
+            }
+            else
+            {
+                Console.WriteLine("so you're welcome");
+
+            }
+
         }
     }
 }
