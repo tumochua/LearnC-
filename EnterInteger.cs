a9d2fe5e48b1e65e1145a0127d78af17e0c7ac86
tumochua <94440913+tumochua@users.noreply.github.com>
2022-10-18 14:58:40 +0700

    so chan le

--- EnterInteger.cs
+++ EnterInteger.cs
@@ -1,39 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
 
-namespace LearnC
-{
-    class HatTwo
-    {
-        static void Main(string[] args)
-        {
-            int result;
-            Console.WriteLine("nhap vao so tu nhien");
-            // Console.ReadLine() nhập vào từ b0àn phím
-            result = Convert.ToInt16(Console.ReadLine());
-            if (result >= 10 || result < 0)
-            {
-                Console.Write("so cua ban nhap ko chinh xac");
-            }
-            else
-            {
-                if (result % 2 == 0)
-                {
-                    Console.Write("so cua ban nhap la so chan");
+// namespace LearnC
+// {
+//     class HatTwo
+//     {
+//         static void Main(string[] args)
+//         {
+//             int result;
+//             Console.WriteLine("nhap vao so tu nhien");
+//             // Console.ReadLine() nhập vào từ b0àn phím
+//             result = Convert.ToInt16(Console.ReadLine());
+//             if (result >= 10 || result < 0)
+//             {
+//                 Console.Write("so cua ban nhap ko chinh xac");
+//             }
+//             else
+//             {
+//                 if (result % 2 == 0)
+//                 {
+//                     Console.Write("so cua ban nhap la so chan");
 
-                }
-                else
-                {
-                    Console.Write("so cua ban nhap la so le");
+//                 }
+//                 else
+//                 {
+//                     Console.Write("so cua ban nhap la so le");
 
 
-                }
-            }
+//                 }
+//             }
 
-        }
+//         }
 
-    }
-}
+//     }
+// }
---
+++ LearnFor.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnC
+{
+    class LearnFor
+    {
+        static void Main(string[] args)
+        {
+            // check so chan le
+            // for (int number = 1; number <= 100; number++)
+            // {
+            //     if (number % 2 != 0)
+            //     {
+            //         Console.Write("\t {0}", number);
+            //     }
+            //     if (number % 10 == 0)
+            //     {
+            //         Console.WriteLine("\n");
+
+            //     }
+            // }
+            // Console.ReadKey();
+
+
+        }
+
+    }
+}
