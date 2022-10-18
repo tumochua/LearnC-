066a1e44e45493e601e433e7ddea8428dddd5a0e
tumochua <94440913+tumochua@users.noreply.github.com>
2022-10-18 15:45:56 +0700

    tam giac

--- LearnFor.cs
+++ LearnFor.cs
@@ -25,6 +25,41 @@ namespace LearnC
             // }
             // Console.ReadKey();
 
+            // tam giac giảm
+
+            int val = 5;
+            int i, j, k;
+            for (i = 1; i <= val; i++)
+            {
+                for (j = 1; j <= val - i; j++)
+                {
+                    // Console.Write(" ");  
+                }
+                for (k = 1; k <= j; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+
+            // tam giac tăng
+            // int val = 5;
+            // int i, j, k;
+            // for (i = 1; i <= val; i++)
+            // {
+            //     for (j = 1; j <= val - i; j++)
+            //     {
+            //         // Console.Write("");  
+            //     }
+            //     for (k = 1; k <= i; k++)
+            //     {
+            //         Console.Write("*");
+            //     }
+            //     Console.WriteLine("");
+            // }
+            // Console.ReadLine();
+
 
         }
 
