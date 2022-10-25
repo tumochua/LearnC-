f882f92c666fd6c9b70c0e3d2aedf934ddd0cf94
tumochua <94440913+tumochua@users.noreply.github.com>
2022-10-25 16:28:37 +0700

    commit

---
+++ Program copy.cs
@@ -0,0 +1,60 @@
+﻿// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+
+// namespace LearnC
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         // nhập số nguyên dương bất kỳ từ bàn phím 1-10 va thông bao ra ten so do
+//         // nhập ngày tháng năm
+//         {
+//             int number;
+//             Console.WriteLine("Nhap vao ngay thang");
+//             number = Convert.ToInt16(Console.ReadLine());
+
+//             switch (number)
+//             {
+//                 case 1:
+//                     Console.WriteLine("thang 1");
+//                     break;
+//                 case 2:
+//                     Console.WriteLine("Thang 2");
+//                     break;
+//                 case 3:
+//                     Console.WriteLine("thang 3");
+//                     break;
+//                 case 4:
+//                     Console.WriteLine("thang 4");
+//                     break;
+//                 case 5:
+//                     Console.WriteLine("thang 5");
+//                     break;
+//                 case 6:
+//                     Console.WriteLine("thang 6");
+//                     break;
+//                 case 7:
+//                     Console.WriteLine("thang 7");
+//                     break;
+//                 case 8:
+//                     Console.WriteLine("thang 8");
+//                     break;
+//                 case 9:
+//                     Console.WriteLine("thang 9");
+//                     break;
+//                 case 10:
+//                     Console.WriteLine("thang 10");
+//                     break;
+//                 default:
+//                     Console.WriteLine("so  ban nhap ko nam trong khoang tu 1 - 10");
+//                     break;
+//             }
+//         }
+
+//     }
+
+
+// }
--- Program.cs
+++ Program.cs
@@ -9,50 +9,56 @@ namespace LearnC
     class Program
     {
         static void Main(string[] args)
-        // nhập số nguyên dương bất kỳ từ bàn phím 1-10 va thông bao ra ten so do
-        // nhập ngày tháng năm
         {
-            int number;
-            Console.WriteLine("Nhap vao ngay thang");
-            number = Convert.ToInt16(Console.ReadLine());
-
-            switch (number)
+            Console.WriteLine("Nhap vao so nguyen lon hon 5");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int tongChan = 0;
+            int tongLe = 0;
+            int tongBa = 0;
+            if (n > 5)
             {
-                case 1:
-                    Console.WriteLine("thang 1");
-                    break;
-                case 2:
-                    Console.WriteLine("Thang 2");
-                    break;
-                case 3:
-                    Console.WriteLine("thang 3");
-                    break;
-                case 4:
-                    Console.WriteLine("thang 4");
-                    break;
-                case 5:
-                    Console.WriteLine("thang 5");
-                    break;
-                case 6:
-                    Console.WriteLine("thang 6");
-                    break;
-                case 7:
-                    Console.WriteLine("thang 7");
-                    break;
-                case 8:
-                    Console.WriteLine("thang 8");
-                    break;
-                case 9:
-                    Console.WriteLine("thang 9");
-                    break;
-                case 10:
-                    Console.WriteLine("thang 10");
-                    break;
-                default:
-                    Console.WriteLine("so  ban nhap ko nam trong khoang tu 1 - 10");
-                    break;
+                Console.WriteLine("\nDanh sach cac so chan tu 1 den {0} la:", n);
+                for (int i = 1; i <= n; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write("\t {0} \t", i);
+                        tongChan += i;
+                    }
+                }
+                Console.WriteLine("\ntong cac so chan tu 1 den {0} la: {1}", n, tongChan);
+
+                Console.WriteLine("\nDanh sach cac so le tu 1 den {0} la:", n);
+                for (int j = 1; j <= n; j++)
+                {
+                    if (j % 2 != 0)
+                    {
+                        Console.Write("\t{0}\t", j);
+                        tongLe += j;
+                    }
+                }
+                Console.WriteLine("\ntong cac so le tu 1 den {0} la: {1}", n, tongLe);
+
+                Console.WriteLine("\nDanh sach cac so chia het cho 3 tu 1 den {0} la:", n);
+                for (int k = 1; k <= n; k++)
+                {
+                    if (k % 3 == 0)
+                    {
+                        Console.Write("\t{0}\t", k);
+                        tongBa += k;
+                    }
+                }
+                Console.WriteLine("\ntong cac so chia het cho 3 tu 1 den {0} la: {1}", n, tongBa);
             }
+            else
+            {
+                Console.WriteLine("\nNhap vao so nguyen lon hon 5");
+            }
+
+
+            Console.ReadKey();
         }
+
 
     }
 
