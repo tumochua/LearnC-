3d747f146fc6029f516bd3837115b49bf03406d0
tumochua <94440913+tumochua@users.noreply.github.com>
2022-11-01 16:15:59 +0700

    commit

--- Program.cs
+++ Program.cs
@@ -10,18 +10,38 @@ namespace LearnC
     {
         static void Main(string[] args)
         {
-            int[] number = new int[5];
+            try
+            {
+                //nhap so phan tu bat ky tu ban phim va in mang
+                Console.Write("Nhap so phan tu cua mang: ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[] arr = new int[n];
+                int i;
+                int sum = 0;
+                for (i = 0; i < n; i++)
+                {
+                    Console.Write("Phan tu - {0} : ", i);
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
 
-            for (int i = 0; i < 5; i++)
-            {
-                Console.Write("number[" + i + "] = ");
-                number[i] = int.Parse(Console.ReadLine());
+                Console.Write("\nIn cac phan tu mang: ");
+                for (i = 0; i < n; i++)
+                {
+                    Console.Write("{0}  ", arr[i]);
+                }
+                for (i = 0; i < n; i++)
+                {
+                    if (arr[i] % 3 == 0)
+                    {
+                        sum = sum + arr[i];
+                    }
+                }
+                Console.Write("\nTong cac so chia het cho 3 la: " + sum);
             }
-            for (int i = 0; i < number.Length; i++)
+            catch
             {
-                Console.Write(number[i] + "    ");
+                Console.Write("Nhap sai! Vui long nhap lai.");
             }
-            Console.ReadKey();
         }
 
 
